d81334ea74c230fd101321f73b654b03bd8992f8
kat3r1 <kascurrarisco@gmail.com>
2023-12-10 06:11:25 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ascurra21100566EF2023"
+include(":app")
+ 
